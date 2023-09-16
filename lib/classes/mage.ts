2a223c64cc014a65693ca4637704352ea75b4f54
Luis <luisfernandogvv@gmail.com>
2023-09-16 09:09:59 -0300

    refactor: Criando arquivos segregados para representação das classes

---
+++ lib/classes/mage.ts
@@ -0,0 +1,15 @@
+import { AbstractPlayerClass } from "../abstracts/classes"
+
+export class Mage extends AbstractPlayerClass {
+    constructor(){
+        super(300)
+    }
+
+    fireBall(){
+
+    }
+
+    frozen(){
+        
+    }
+}
---
+++ lib/classes/warrior.ts
@@ -0,0 +1,13 @@
+import { AbstractPlayerClass } from "../abstracts/classes";
+
+export class Warrior extends AbstractPlayerClass {
+
+    constructor(){
+        super(100)
+    }
+
+    swordAttack(){
+
+    }
+
+}
